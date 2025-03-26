d2e3bf29bae18031e7141a8b68c43d0a5c674c51
Matyáš Strelec <matyasstrelec@gmail.com>
2025-03-26 22:09:50 +0100

    create `find_route` endpoint - returns empty response now

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/find_route_by_coord")
+async def find_route_by_coord():
+        
+    return {"streets_coord": [],
+            "route": list(),
+            "src_street": "",
+            "dst_street": "",
+    }
